c5b2e75f484b2232bf964eeeaa5b0b05de2106b6
rekan <rekanagy06@gmail.com>
2016-05-18 16:21:14 +0200

    all boxes moving down

--- enemy.py
+++ enemy.py
@@ -15,24 +15,26 @@ screen.keypad(1)
 boxes = []
 
 def box_move():
-    for j in range (1,5):
+    for j in range (len(boxes)):
+        screen.erase()
+        screen.border(0)
         for i in boxes:
+            screen.refresh()
             box = curses.newwin(3, len(text)+2, 3*j, i[0])
             box.box()
             box.addstr(1, 1, i[1])
-            screen.refresh()
             box.refresh()
         sleep(0.2)
 
 for i in range(5):
-    text = "aaa"
+    text = "a"
     x_pos = random.randrange(1, 74)
     box = curses.newwin(3, len(text)+2, 0, x_pos)
     box.box()
     box.addstr(1, 1, text)
-    boxes.append([x_pos, text])
     screen.refresh()
     box.refresh()
+    boxes.append([x_pos, text])
     sleep(0.2)
 
 box_move()
