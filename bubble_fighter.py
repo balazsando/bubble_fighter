452aa37887dcf1faa41a77c2f21e1e14053865e7
Balazs Ando <elnesia@gmail.com>
2016-05-18 10:54:24 +0200

    beginning...

--- bubble_fighter.py
+++ bubble_fighter.py
@@ -1,3 +1,9 @@
 import curses
 
-screen = curses.initscr()
+def main(startscr):
+    field = curses.newwin(5, 5, 0, 0)
+
+    game_over = 'Game Over!'
+    win.addstr(curses.LINES // 2, (curses.COLS - len(game_over)) // 2, game_over)
+
+curses.wrapper(main)
