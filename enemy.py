8fd3592cec30b739b9e5ad30b07abe7e8a1e6e1a
rekan <rekanagy06@gmail.com>
2016-05-18 16:40:33 +0200

    speed of box movement refined

--- enemy.py
+++ enemy.py
@@ -20,24 +20,25 @@ def box_move():
         screen.border(0)
         for i in boxes:
             screen.refresh()
-            box = curses.newwin(3, len(text)+2, 3*j, i[0])
+            i[1] += i[3]
+            box = curses.newwin(3, len(text)+2, i[1], i[0])
             box.box()
-            box.addstr(1, 1, i[1])
+            box.addstr(1, 1, i[2])
             box.refresh()
         sleep(0.2)
 
-for i in range(5):
+for i in range(8):
     text = "a"
     x_pos = random.randrange(1, 74)
+    speed = random.randrange(1,3)
     box = curses.newwin(3, len(text)+2, 0, x_pos)
     box.box()
     box.addstr(1, 1, text)
     screen.refresh()
     box.refresh()
-    boxes.append([x_pos, text])
-    sleep(0.2)
+    boxes.append([x_pos, 0, text, speed])
+    box_move()
 
-box_move()
 
 while True:
     event = screen.getch()
