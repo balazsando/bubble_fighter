5dcba2f8178443bcd933370d9c2b984802b8ad22
Balazs Ando <elnesia@gmail.com>
2016-05-18 11:41:48 +0200

    making progress

--- bubble_fighter.py
+++ bubble_fighter.py
@@ -4,11 +4,21 @@
 import curses
 
 screen = curses.initscr()
+screen.border(0)
 curses.noecho()
 curses.curs_set(0)
 screen.keypad(1)
 
-screen.addstr("This is a Sample Curses Script\n\n")
+text = "This is a Sample Curses Script"
+box1 = curses.newwin(10, len(text)+2, 0, 0)
+box1.box()
+
+
+screen.refresh()
+box1.refresh()
+box1.addstr(1, 1, text)
+box1.refresh()
+
 while True:
    event = screen.getch()
    if event == ord("q"): break
