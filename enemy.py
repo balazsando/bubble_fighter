bab0ca7ed2d077d652c8f46d6edcf5292b66bfc0
rekan <rekanagy06@gmail.com>
2016-05-19 15:06:56 +0200

    committing to be able to pull updated version

--- enemy.py
+++ enemy.py
@@ -16,6 +16,7 @@ curses.curs_set(0)
 screen = curses.newwin(curses.LINES, curses.COLS, 0, 0)
 
 box_content = []
+#box_blocks = []
 score = 0
 life = 3
 
@@ -33,35 +34,40 @@ def box_reach_end(i):
     health-=1
     kill_enemy(i)
 
-boxes = []
 
 def box_move():
     for j in range (5):
+        screen.clear()
         screen.border(0)
+        #count = 0
         event = screen.getch()
         if event != -1:
             key_pressed(chr(event))
         for i in box_content:
-            screen.refresh()
+            #screen.refresh()
             i.y_pos += i.speed
-            box = curses.newwin(3, len(i.text)+2, i.y_pos, i.x_pos)
-            box.box()
-            box.addstr(1, 1, i.text)
-            box.refresh()
-            if i.y_pos > curses.LINES-3:        #add here LINES coordinate
-                sleep(0.1)#add box reach end
+            screen.addstr(i.y_pos, i.x_pos, i.text)
+            #print(i.y_pos, i.x_pos)
+            #box_blocks[count].mvderwin(i.y_pos, i.x_pos)
+            #box_blocks[count].refresh()
+            screen.refresh()
+            #count += 1
+            #box.box()
+            #box.addstr(1, 1, i.text)
+            #if i.y_pos > curses.LINES-3:        #add here LINES coordinate
+            #    sleep(0.1)#add box reach end
         sleep(0.2)
 
 while score != 20:
     screen.timeout(100)
     clone = riddle.create_riddle()
-    box = curses.newwin(3, len(clone.text)+2, clone.y_pos, clone.x_pos)
-    box.box()
-    box.addstr(1, 1, clone.text)
+    #box = screen.derwin(3, len(clone.text)+2, clone.y_pos, clone.x_pos)
+    #box.box()
+    screen.addstr(clone.y_pos, clone.x_pos, clone.text)
     screen.refresh()
-    box.refresh()
+    #box.refresh()
     box_content.append(clone)
-
+    #box_blocks.append(box)
     box_move()
 
 
