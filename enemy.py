490c9f196f91e9aefd7b43221c0f0100ae12b0e4
Balazs Ando <elnesia@gmail.com>
2016-05-19 15:02:49 +0200

    loads of patches

--- enemy.py
+++ enemy.py
@@ -9,15 +9,19 @@ import riddle
 
 
 curses.initscr()
+curses.start_color()
+curses.use_default_colors()
+curses.init_pair(1, 0, 7)
 
 curses.noecho()
 curses.curs_set(0)
 
 screen = curses.newwin(curses.LINES, curses.COLS, 0, 0)
+screen.timeout(10)
 
 box_content = []
 score = 0
-life = 3
+life = 5
 
 def kill_enemy(index):
     box_content.pop(index)
@@ -28,12 +32,12 @@ def key_pressed(key):
             global score
             score+=1
             kill_enemy(i)
+            break
 
 def box_reach_end(i):
-    health-=1
+    global life
+    life-=1
     kill_enemy(i)
-
-boxes = []
 
 def box_move():
     for j in range (5):
@@ -41,32 +45,37 @@ def box_move():
         event = screen.getch()
         if event != -1:
             key_pressed(chr(event))
+        count = 0
         for i in box_content:
-            screen.refresh()
-            i.y_pos += i.speed
+            game_progress = curses.newwin(3, curses.COLS, 0, 0)
+            game_progress.box()
+            game_progress.addstr(1, 1, "SCORE: "+str(score))
+            lifes = ""
+            for l in range(life):
+                lifes += "💚 "
+            for l in range(5-life):
+                lifes += "💀 "
+            lifes += str(life)
+            life_text = "LIFE: "+lifes
+            game_progress.addstr(1, curses.COLS-20, life_text)
+            game_progress.refresh()
             box = curses.newwin(3, len(i.text)+2, i.y_pos, i.x_pos)
-            box.box()
+            box.attrset(curses.color_pair(1))
             box.addstr(1, 1, i.text)
+            box.box()
             box.refresh()
-            if i.y_pos > curses.LINES-3:        #add here LINES coordinate
-                sleep(0.1)#add box reach end
+            screen.refresh()
+            if i.y_pos > curses.LINES-5:        #add here LINES coordinate
+                box_reach_end(count)
+            i.y_pos += i.speed
+            count+=1
         sleep(0.2)
 
-while score != 20:
-    screen.timeout(100)
+
+
+while life > 0:
     clone = riddle.create_riddle()
-    box = curses.newwin(3, len(clone.text)+2, clone.y_pos, clone.x_pos)
-    box.box()
-    box.addstr(1, 1, clone.text)
-    screen.refresh()
-    box.refresh()
     box_content.append(clone)
-
     box_move()
-
-
-while True:
-    event = screen.getch()
-    if event == ord('q'): break
 
 curses.endwin()
