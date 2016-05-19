21a102dfcf535131659606929501832da6efb1a3
Balazs Ando <elnesia@gmail.com>
2016-05-19 22:33:23 +0200

    working menu

--- enemy.py
+++ enemy.py
@@ -11,12 +11,13 @@ import riddle
 curses.initscr()
 curses.start_color()
 curses.use_default_colors()
-curses.init_pair(1, 0, 7)
+curses.init_pair(5, 0, 7)
 
 curses.noecho()
 curses.curs_set(0)
 
 screen = curses.newwin(curses.LINES, curses.COLS, 0, 0)
+screen.nodelay(1)
 screen.timeout(10)
 
 box_content = []
@@ -27,7 +28,7 @@ def kill_enemy(index):
     box_content.pop(index)
 
 def key_pressed(key):
-    for i in range(len(box_content)-1):
+    for i in range(len(box_content)):
         if box_content[i].result == key:
             global score
             score+=1
@@ -44,38 +45,45 @@ def box_move():
         screen.border(0)
         event = screen.getch()
         if event != -1:
+            if event == 27:
+                global life
+                life=0
+                break
             key_pressed(chr(event))
         count = 0
         for i in box_content:
             game_progress = curses.newwin(3, curses.COLS, 0, 0)
             game_progress.box()
             game_progress.addstr(1, 1, "SCORE: "+str(score))
+            title = "BUBBLE - FIGHTER 1.0"
+            game_progress.addstr(1, (curses.COLS - len(title)) //2, title)
             lifes = ""
-            for l in range(life):
-                lifes += "💚 "
-            for l in range(5-life):
-                lifes += "💀 "
+            for l in range(life): lifes += "💚 "
+            for l in range(5-life): lifes += "💀 "
             lifes += str(life)
             life_text = "LIFE: "+lifes
             game_progress.addstr(1, curses.COLS-20, life_text)
             game_progress.refresh()
             box = curses.newwin(3, len(i.text)+2, i.y_pos, i.x_pos)
-            box.attrset(curses.color_pair(1))
+            box.attrset(curses.color_pair(5))
             box.addstr(1, 1, i.text)
             box.box()
             box.refresh()
-            screen.refresh()
-            if i.y_pos > curses.LINES-5:        #add here LINES coordinate
+            if i.y_pos > curses.LINES-5:
                 box_reach_end(count)
+                if life == 0: break
             i.y_pos += i.speed
             count+=1
         sleep(0.2)
 
-
-
-while life > 0:
-    clone = riddle.create_riddle()
-    box_content.append(clone)
-    box_move()
-
-curses.endwin()
+def game_start():
+    global life
+    global score
+    global box_content
+    life = 5
+    score = 0
+    while life > 0:
+        clone = riddle.create_riddle()
+        box_content.append(clone)
+        box_move()
+    box_content.clear()
--- welcome.py
+++ welcome.py
@@ -1,54 +1,74 @@
 import curses
-import time
+import enemy
+from curses import KEY_UP, KEY_DOWN
 from time import sleep
-from curses import COLOR_RED
-from curses import KEY_UP, KEY_DOWN, KEY_ENTER
 
-screen = curses.initscr()
+curses.initscr()
 curses.start_color()
 curses.use_default_colors()
-screen.border(0)
+screen = curses.newwin(curses.LINES, curses.COLS, 0, 0)
 curses.noecho()
 curses.curs_set(0)
 screen.keypad(1)
-
+welcome1 = "WELCOME TO RIDDLE FIGHTER 1.0"
+welcome2 = "       1 - PLAYER        "
+welcome3 = "2 - PLAYERS (coming soon)"
+welcome4 = "Destroy enemies falling from the sky by solving their riddles!    "
+welcome5 = "Solve the math problem or enter the letter to make them disappear."
+welcome6 = "      - POINTS are awarded for each enemy killed.                 "
+welcome7 = "      - You lose LIFE if enemies reach the ground.                "
+gameover1 = "Traceback (most recent call last):                  "
+gameover2 = "      File \"python/bubble_fighter/welcome.py\", line 20, in <module>"
+gameover3 = "                                         sdfwefw"
+gameover4 = "                                 NameError: name 'sdfw"
+gameover5 = "Just jokin... you got REKT!"
+curses.init_pair(1,-1,1)
+curses.init_pair(2,-1,3)
+curses.init_pair(3,1,-1)
+curses.init_pair(4,7,0)
 
 def welcome_screen():
-    welcome1 = "WELCOME TO RIDDLE FIGHTER 1.0"
-    welcome2 = "       1 - PLAYER        "
-    welcome3 = "2 - PLAYERS (coming soon)"
-    welcome4 = "Destroy enemies falling from the sky by solving their riddles!    "
-    welcome5 = "Solve the math problem or enter the letter to make them disappear."
-    welcome6 = "      - POINTS are awarded for each enemy killed.                 "
-    welcome7 = "      - You lose LIFE if enemies reach the ground.                "
     game_mode = 1
-    curses.init_pair(1,-1,1)
-    curses.init_pair(2,-1,3)
-    curses.init_pair(3,1,-1)
-    curses.init_pair(4,7,0)
-    screen.addstr(curses.LINES // 2 - 5, (curses.COLS - len(welcome1)) // 2, welcome1, curses.color_pair(1))
     screen.addstr(curses.LINES // 2 - 3, (curses.COLS - len(welcome2)) // 2, welcome2, curses.color_pair(2))
     screen.addstr(curses.LINES // 2 - 1, (curses.COLS - len(welcome3)) // 2, welcome3, curses.color_pair(3))
-    screen.addstr(curses.LINES // 2 + 4, (curses.COLS - len(welcome4)) // 2, welcome4, curses.color_pair(4))
-    screen.addstr(curses.LINES // 2 + 5, (curses.COLS - len(welcome5)) // 2, welcome5, curses.color_pair(4))
-    screen.addstr(curses.LINES // 2 + 6, (curses.COLS - len(welcome6)) // 2, welcome6, curses.color_pair(4))
-    screen.addstr(curses.LINES // 2 + 7, (curses.COLS - len(welcome7)) // 2, welcome7, curses.color_pair(4))
     while True:
-        event = screen.getch()
-        if event == KEY_DOWN and game_mode == 1:
+        screen.addstr(curses.LINES // 2 - 5, (curses.COLS - len(welcome1)) // 2, welcome1, curses.color_pair(1))
+        screen.addstr(curses.LINES // 2 + 4, (curses.COLS - len(welcome4)) // 2, welcome4, curses.color_pair(4))
+        screen.addstr(curses.LINES // 2 + 5, (curses.COLS - len(welcome5)) // 2, welcome5, curses.color_pair(4))
+        screen.addstr(curses.LINES // 2 + 6, (curses.COLS - len(welcome6)) // 2, welcome6, curses.color_pair(4))
+        screen.addstr(curses.LINES // 2 + 7, (curses.COLS - len(welcome7)) // 2, welcome7, curses.color_pair(4))
+        select = screen.getch()
+        if select == KEY_DOWN:
             screen.addstr(curses.LINES // 2 - 3, (curses.COLS - len(welcome2)) // 2, welcome2, curses.color_pair(3))
             screen.addstr(curses.LINES // 2 - 1, (curses.COLS - len(welcome3)) // 2, welcome3, curses.color_pair(2))
             game_mode = 2
-            screen.refresh()
-        if event == KEY_UP and game_mode == 2:
+        if select == KEY_UP:
             screen.addstr(curses.LINES // 2 - 3, (curses.COLS - len(welcome2)) // 2, welcome2, curses.color_pair(2))
             screen.addstr(curses.LINES // 2 - 1, (curses.COLS - len(welcome3)) // 2, welcome3, curses.color_pair(3))
             game_mode = 1
-            screen.refresh()
-        if event == 10 and game_mode == 1:
+        if select == 10 and game_mode == 1:
             screen.addstr(curses.LINES // 2 - 5, (curses.COLS - len(welcome1)) // 2, welcome1, curses.color_pair(2))
+            enemy.game_start()
+            screen.erase()
+            screen.addstr(curses.LINES // 3 - 1, curses.COLS // 2, gameover1)
+            screen.addstr(curses.LINES // 3, curses.COLS // 2, gameover2)
+            screen.addstr(curses.LINES // 3 + 1, curses.COLS // 2, gameover3)
+            screen.addstr(curses.LINES // 3 + 2, curses.COLS // 2, gameover4)
+            screen.refresh()
+            sleep(2)
+            screen.erase()
+            screen.addstr(curses.LINES // 2, (curses.COLS -len(gameover5)) // 2, gameover5)
+            screen.refresh()
+            sleep(3)
+            screen.erase()
+            screen.addstr(curses.LINES // 2, (curses.COLS - 1) // 2, "nub")
+            screen.refresh()
+            sleep(1)
+            screen.erase()
+            screen.addstr(curses.LINES // 2 - 3, (curses.COLS - len(welcome2)) // 2, welcome2, curses.color_pair(2))
+            screen.addstr(curses.LINES // 2 - 1, (curses.COLS - len(welcome3)) // 2, welcome3, curses.color_pair(3))
+        if select == 27:
+            curses.endwin()
             break
 
-
 welcome_screen()
-curses.endwin()
